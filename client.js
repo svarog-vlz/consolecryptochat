eb08d27b3ac4bb66cda771db4cec31033d5dee98
Смирнов А.А <fryggr@gmail.com>
2018-04-28 21:55:09 +0300

    Исправил ошибки, убрал лишний код, добавил коментарии

--- client.js
+++ client.js
@@ -2,51 +2,56 @@ const net=require("net");
 const NodeRSA = require('node-rsa');
 const readline = require('readline');
 const color = require('colors');
+
 let client= new net.Socket();
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 const port = process.argv[2];
-const host = process.argv[3] || 'localhost';
-const username =  process.argv[4] || 'Anonymous'
+const host = process.argv[3] || 'localhost'; // localhost - по умолчанию
+const username =  process.argv[4] || 'Anonymous' // Anonymous - по умолчанию
 
-
+// Создаем обьекты RSA для себя и сервера
 let serverRSA  = new NodeRSA({b: 512});
 let clientRSA  = new NodeRSA({b: 512});
 
 
 client.connect(port, host, function(){
-    client.name = client.remoteAddress;
-	console.log(`Connected to ${color.red(client.name)}`);
+	console.log(`Connected to ${color.red(client.remoteAddress)}`);
+	//Отправляем свой публичный ключ на сервер
 	client.write(clientRSA.exportKey('public'))
 });
 
 
 client.on("data",function(data){
+	//Если переданные данные являются публичным ключем
 	if(isPuplicKey(data)) {
-			console.log(`Encrypted connection established \n`);
+			//Импортируем серверный ключ в ранее созданный обьект
 			serverRSA.importKey(data,'pkcs8-public');
-			//console.log(color.bgYellow.black(data.toString()));
+			console.log(`Encrypted connection established \n`);
 		 } else {
-
 		 	message = JSON.parse(data);
+			//Расшифровываем собщение своим публичным ключом
 		 	msg = clientRSA.decrypt(message.msg, 'utf8');
+			//Проверяем цифровую подпись сообщения
 		 	if(serverRSA.verify(msg.toString(), message.sign.toString(), 'utf8', 'base64')) {
 		 		console.log(color.green(msg));
-		 	} else console.log(color.red("NOT VERYFED:"+msg));
+		 	} else console.log(color.red("NOT VERYFED:"+msg)); 
 		 }
 			 
 		
     
 })
 
-
+//Отправка сообщений
 function sendMsg(msg){
-
+	//Создаем цифровую подпись
 	let sign = clientRSA.sign(msg, 'base64');
+	//Упаковываем в обьект сообщение и ключ
 	let mes =  {
 		msg: serverRSA.encrypt(msg, 'base64'),
 		sign: sign
 	}
+	//Отправляем в json
     client.write(JSON.stringify(mes));
 }
 
@@ -54,7 +59,7 @@ rl.on('line', function (line) {
     sendMsg(color.red(username+": ")+line);
 });
 
-
+//Эту фукнцию надо явно преписать
 function isPuplicKey (key) {
 	if(key.indexOf("BEGIN PUBLIC KEY-----")>0) {
 		return true;
--- server.js
+++ server.js
@@ -4,30 +4,37 @@ const readline = require('readline');
 const color = require('colors');
 
 const port = process.argv[2];
-const username =  process.argv[3] || 'Anonymous'
+const username =  process.argv[3] || 'Anonymous' // Anonymous - по умолчанию
+
+const rl = readline.createInterface(process.stdin, process.stdout);
 
 let socket;
 let client;
+
 console.log ("Server start, and waitng client on "+ color.red(port)+" port")
-const rl = readline.createInterface(process.stdin, process.stdout);
+
+// Создаем обьекты RSA для себя и сервера
 let serverRSA  = new NodeRSA({b: 512});
 let clientRSA = new NodeRSA({b: 512});
+
 net.createServer(function(socket){
-    socket.name = socket.remoteAddress;
-    console.log(color.red(socket.name)+' connected');
+    console.log(color.red(socket.remoteAddress)+' connected');
     socket.setEncoding('utf8');
     client = socket;
     socket.on("data",function(data){	
-		 //если нам прислали публичный ключ, записываем его и посылаем свой. 
+		 //Если переданные данные являются публичным ключем 
 		 if(isPuplicKey(data)) {
-			//console.log(color.bgYellow.black(data.toString()));
+			 //Импортируем клиентский ключ в ранее созданный обьект
 			clientRSA.importKey(data, 'pkcs8-public');
+			//Отправляем свой публичный ключ клиенту
 			socket.write(serverRSA.exportKey('public'));
 			console.log(`Encrypted connection established \n`);
 		 }
 		 else {
 		 	message = JSON.parse(data);
+			//Расшифровываем собщение своим публичным ключом
 		 	msg = serverRSA.decrypt(message.msg, 'utf8');
+			//Проверяем цифровую подпись сообщения
 		 	if(clientRSA.verify(msg.toString(), message.sign.toString(), 'utf8', 'base64')) {
 		 		console.log(color.green(msg));
 		 	} else console.log(color.red("NOT VERYFED:"+msg));
@@ -36,18 +43,20 @@ net.createServer(function(socket){
     });
 
     socket.on("close",function(){
-        console.log(socket.name + " has disconnected");
+        console.log(socket.remoteAddress + " has disconnected");
     })
 
 }).listen(port);
-
+//Отправка сообщений
 function sendMsg(msg){
+	//Создаем цифровую подпись
 	let sign = serverRSA.sign(msg, 'base64');
+	//Упаковываем в обьект сообщение и ключ
 	let mes =  {
 		msg: clientRSA.encrypt(msg, 'base64'),
 		sign: sign
 	}
-
+	//Отправляем в json
     client.write( JSON.stringify(mes));
 }
 
@@ -55,7 +64,7 @@ rl.on('line', function (line) {
 	 sendMsg(color.red(username+": ")+line); 
 });
 
-//Пока такой тупой сопосб проверить что переданное значение явлется ключем
+//Эту фукнцию надо явно преписать
 function isPuplicKey (key) {
 	if(key.indexOf("BEGIN PUBLIC KEY-----")>0) {
 		return true;
